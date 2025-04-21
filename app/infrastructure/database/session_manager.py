5aef02ea0f15d16e6398ed98de9f48070b52065d
Pedro Paulo <42193455+pedropauloo@users.noreply.github.com>
2025-04-21 13:47:03 -0300

    feat: refactor DatabaseSessionManager to use SingletonMeta for singleton pattern

--- app/infrastructure/database/session_manager.py
+++ app/infrastructure/database/session_manager.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config.settings import settings
+from app.infrastructure.singleton_meta import SingletonMeta
 
 
-class DatabaseSessionManager:
+class DatabaseSessionManager(metaclass=SingletonMeta):
     def __init__(self):
         self.engine = create_engine(settings.DATABASE_URL, echo=True)
         self.SessionLocal = sessionmaker(bind=self.engine)
