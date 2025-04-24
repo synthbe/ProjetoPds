322793a20b91a5aab01e79ad5389ada5a6164f2a
synthbe <108361383+synthbe@users.noreply.github.com>
2025-04-24 10:53:39 -0300

    feat: improve repository generalization

--- app/repositories/audio_repository.py
+++ app/repositories/audio_repository.py
@@ -1,55 +1,14 @@
-from sqlalchemy.orm import Session
-from typing import List
-
 from models.audio_model import Audio
 from schemas.audio_schema import AudioCreate, AudioUpdate
-from abstract_repository import AbstractRepository
+from repository import Repository
 
-class AudioRepository(AbstractRepository[Audio, AudioCreate, AudioUpdate]):
-    def __init__(self, db: Session):
-        self.db = db
+class AudioRepository(Repository[Audio, AudioCreate, AudioUpdate]):
+    @property
+    def model(self) -> type[Audio]:
+        return Audio
 
-    def create(self, obj_data: AudioCreate) -> Audio:
-        audio = Audio(
-            name=obj_data.name,
-            pinned=False,
-        )
-        self.db.add(audio)
-        self.db.commit()
-        self.db.refresh(audio)
-        return audio
-
-    def get_by_id(self, obj_id: int) -> Audio | None:
-        return self.db.query(Audio).filter(Audio.id == obj_id).first()
-
-    def get_all(self) -> List[Audio]:
-        return self.db.query(Audio).all()
-
-    def update(self, obj_data: AudioUpdate, obj_id: int) -> Audio | None:
-        audio = self.get_by_id(obj_id)
-        if not audio:
-            return None
-
-        if obj_data.name is not None:
-            audio.name = obj_data.name
-
-        self.db.commit()
-        self.db.refresh(audio)
-
-        return audio
-
-    def delete(self, obj_id: int) -> Audio | None:
-        audio = self.get_by_id(obj_id)
-        if not audio:
-            return None
-
-        self.db.delete(audio)
-        self.db.commit()
-
-        return audio
-
-    def toggle_audio_pin(self, obj_id: int) -> Audio | None:
-        audio = self.get_by_id(obj_id)
+    def toggle_audio_pin(self, id: int) -> Audio | None:
+        audio = self.get_by_id(id)
         if not audio:
             return None
         audio.pinned = not audio.pinned
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,19 +1,8 @@
-import uuid
+from models.user_model import User
+from schemas.user_schema import UserCreate, UserUpdate
+from repository import Repository
 
-from app.models.user_model import User
-from .repository import Repository
-
-
-class UserRepository(Repository):
-    def get_by_id(self, id: uuid.UUID):
-        return self.db.query(User).filter(User.id == id).first()
-
-    def get_by_email(self, email: str):
-        return self.db.query(User).filter(User.email == email).first()
-
-    def create(self, user: User):
-        self.db.add(user)
-        self.db.commit()
-        self.db.refresh(user)
-
-        return user
+class UserRepository(Repository[User, UserCreate, UserUpdate]):
+    @property
+    def model(self) -> type[User]:
+        return User
