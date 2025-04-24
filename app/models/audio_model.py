3ca0d97df96fbd20e19c0a18785940803b5dde0d
synthbe <108361383+synthbe@users.noreply.github.com>
2025-04-24 10:52:32 -0300

    feat: user and audio model linked and new attributes

--- app/models/audio_model.py
+++ app/models/audio_model.py
@@ -1,17 +1,18 @@
+from sqlalchemy import ForeignKey, String, TIMESTAMP, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Boolean, ForeignKey, String, TIMESTAMP
-from datetime import datetime
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
+from app.config import BaseModel
 
-from app.db import Base
-
-class Audio(Base):
+class Audio(BaseModel):
     __tablename__ = "audios"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    date_in: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
-    date_modified: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    date_in: Mapped[str] = mapped_column(TIMESTAMP, server_default=func.now())
+    date_modified: Mapped[str] = mapped_column(TIMESTAMP, server_default=func.now())
+    data_path: Mapped[str] = mapped_column(String, nullable=False)
     pinned: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    user: Mapped["User"] = relationship(back_populates="audios")
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    owner: Mapped["User"] = relationship(back_populates="audios")
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,10 +1,9 @@
 import uuid
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import UUID as pgUUID
 
 from app.config import BaseModel
-
 
 class User(BaseModel):
     __tablename__ = "users"
@@ -15,3 +14,5 @@ class User(BaseModel):
     name: Mapped[str] = mapped_column(String(50))
     email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255))
+
+    audios: Mapped[list["Audio"]] = relationship(back_populates="owner", cascade="all, delete")
