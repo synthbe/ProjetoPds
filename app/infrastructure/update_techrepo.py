87eaab66215f1830afc319c78d3425fb877fd02e
Pedro Paulo <42193455+pedropauloo@users.noreply.github.com>
2025-05-02 00:18:07 -0300

    refactor: update settings and sync_music_model_repo to use AUDIO_EXTRACTOR_REPO_URL and AUDIO_EXTRACTOR_REPO_DIR

--- app/infrastructure/update_techrepo.py
+++ app/infrastructure/update_techrepo.py
@@ -4,23 +4,34 @@ import git
 from git.exc import GitCommandError
 from app.config.settings import settings
 
-# clona ou puxa o repositório do modelo de música e instala as dependências
 
 def sync_music_model_repo():
-    if not os.path.exists(settings.CLONE_DIR):
-        print(f"⏬ clonando repo de {settings.REPO_URL} pra pasta '{settings.CLONE_DIR}'...")
-        git.Repo.clone_from(settings.REPO_URL, settings.CLONE_DIR)
+    if not os.path.exists(settings.AUDIO_EXTRACTOR_REPO_DIR):
+        print(
+            f"⏬ clonando repo de {settings.AUDIO_EXTRACTOR_REPO_URL} pra pasta '{settings.AUDIO_EXTRACTOR_REPO_DIR}'..."
+        )
+        git.Repo.clone_from(
+            settings.AUDIO_EXTRACTOR_REPO_URL, settings.AUDIO_EXTRACTOR_REPO_DIR
+        )
         print("✅ clonagem feita")
 
         # instala as dependências que tão no requirements.txt
         print("🔧 instalando dependências do requirements.txt do repo...")
-        subprocess.run(["uv", "pip", "install", "-r", os.path.join(settings.CLONE_DIR, "requirements.txt")])
+        subprocess.run(
+            [
+                "uv",
+                "pip",
+                "install",
+                "-r",
+                os.path.join(settings.AUDIO_EXTRACTOR_REPO_DIR, "requirements.txt"),
+            ]
+        )
         print("✅ dependências instaladas")
     else:
         print("🔄 repo já existe, tentando puxar atualizações...")
 
         try:
-            repo = git.Repo(settings.CLONE_DIR)
+            repo = git.Repo(settings.AUDIO_EXTRACTOR_REPO_DIR)
             origin = repo.remotes.origin
             origin.fetch()  # pega os commits novos do remoto
             origin.pull()  # puxa as mudanças pro diretório atual
@@ -28,11 +39,19 @@ def sync_music_model_repo():
         except GitCommandError as e:
             print(f"⚠️ deu erro ao tentar atualizar o repo: {e}")
             print("fazendo reset total pro que tá no remoto...")
-            repo.git.reset('--hard')  # reseta tudo pras últimas alterações do remoto
+            repo.git.reset("--hard")  # reseta tudo pras últimas alterações do remoto
             origin.pull()  # puxa dnv depois do reset
             print("✅ reset feito e repo atualizado")
 
     # sempre instala as dependências dps de clonar ou atualizar
     print("🔧 instalando dependências do requirements.txt do repo...")
-    subprocess.run(["uv", "pip", "install", "-r", os.path.join(settings.CLONE_DIR, "requirements.txt")])
+    subprocess.run(
+        [
+            "uv",
+            "pip",
+            "install",
+            "-r",
+            os.path.join(settings.AUDIO_EXTRACTOR_REPO_DIR, "requirements.txt"),
+        ]
+    )
     print("✅ dependências instaladas")
