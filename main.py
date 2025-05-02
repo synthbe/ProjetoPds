cdbc8a5633c52f73f04e60d592707020cdd51d65
luwi <cnomeedson@yahoo.com>
2025-05-02 00:29:06 -0300

    refactored repo update code into OOP model

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from app.infrastructure import DatabaseInitializer
 from app.config.middleware import MiddlewareManager
 from app.router import Router
 from app.config.swagger import SwaggerConfig
-from app.infrastructure.update_techrepo import sync_music_model_repo
+from ProjetoPds.app.infrastructure.techrepo.creator import MusicModelRepoCreator 
 
 DatabaseInitializer.run()
 
 app = FastAPI()
 
 MiddlewareManager(app).setup()
-
 Router(app).register()
-
 app.openapi = SwaggerConfig().setup(app)
 
 @app.on_event("startup")
 async def startup_event():
-    sync_music_model_repo()
+    repo_manager = MusicModelRepoCreator.create()
+    repo_manager.sync()                          
