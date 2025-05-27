6d76d3201e6e86556a40fac8adb2d66530aae36c
edson <wsdfghj@fgh.com>
2025-05-26 21:01:24 -0300

    feat: finished extraction model pipelining

--- app/facade/audio_inference_facade.py
+++ app/facade/audio_inference_facade.py
@@ -2,39 +2,43 @@ import logging
 import subprocess
 import sys
 from pathlib import Path
-from typing import Dict, List, Literal
+from typing import Dict, List
 
 from app.config.settings import settings
 
+MODEL_CONFIG_MAP = {
+    "vocals": {
+        "yaml": "artifacts/melbandroformers/vocals/voc_gabox.yaml",
+        "ckpt": "artifacts/melbandroformers/vocals/voc_fv5.ckpt",
+    },
+    "4stems": {
+        "yaml": "artifacts/melbandroformers/4stems/4stems.yaml",
+        "ckpt": "artifacts/melbandroformers/4stems/4stems.ckpt",
+    },
+    "noise_reduction": {
+        "yaml": "artifacts/melbandroformers/test2/voc_gabox.yaml",
+        "ckpt": "artifacts/melbandroformers/test2/voc_fv5.ckpt",
+    },
+    # Add more models as needed
+}
+
 class AudioInference:
     @staticmethod
-    def vocal_inference(
-        audio_path: str,
-        extraction_type: Literal["vocals", "instrumental", "4stems"]
-    ) -> List[Dict[str, str]]:
+    def pipeline_inference(audio_path: str, model_key: str) -> List[Dict[str, str]]:
         logging.debug(f"audio_path = {audio_path}")
-        logging.debug(f"extraction_type = {extraction_type}")
+        logging.debug(f"model_key = {model_key}")
 
-        # Mapear extraction_type para o arquivo de config correto
-        # Colocar mapeamento em um lugar mais adequado
-        config_map = {
-            "vocals": { "yaml": "artifacts/melbandroformers/vocals/voc_gabox.yaml", "ckpt": "artifacts/melbandroformers/vocals/voc_fv5.ckpt" },
-            "4stems": {}, # Define
-        }
-
-        # ALERT! SERVER RETURNS 200 IF FILES ABOVE NOT FOUND
-
-        config_filename = config_map[extraction_type].get("yaml")
-        if config_filename is None:
-            raise ValueError(f"Unsupported extraction type: {extraction_type}")
+        config = MODEL_CONFIG_MAP.get(model_key)
+        if not config:
+            raise ValueError(f"Unsupported model in pipeline: {model_key}")
 
         args = [
             sys.executable,
             f"{settings.AUDIO_EXTRACTOR_REPO_DIR}/inference.py",
             "--config_path",
-            f"{config_map[extraction_type]['yaml']}",
+            config["yaml"],
             "--start_check_point",
-            f"{config_map[extraction_type]['ckpt']}",
+            config["ckpt"],
             "--input_folder",
             audio_path,
             "--store_dir",
@@ -43,7 +47,7 @@ class AudioInference:
             "mel_band_roformer",
         ]
 
-        subprocess.run(args)
+        subprocess.run(args, check=True)
 
         output_files = []
 
@@ -51,15 +55,13 @@ class AudioInference:
         for file in input_folder.iterdir():
             if not file.is_file():
                 continue
-            output_folder_name = file.stem  # nome sem extensão
+            output_folder_name = file.stem
             output_folder_path = input_folder / output_folder_name
             if not output_folder_path.exists() or not output_folder_path.is_dir():
                 continue
             for generated_file in output_folder_path.iterdir():
-                output_files.append(
-                    {
-                        "name": generated_file.name,
-                        "path": str(generated_file.resolve()),
-                    }
-                )
+                output_files.append({
+                    "name": generated_file.name,
+                    "path": str(generated_file.resolve()),
+                })
         return output_files
--- app/services/audio_service.py
+++ app/services/audio_service.py
@@ -1,7 +1,6 @@
 import shutil
 from uuid import UUID, uuid4
 from pathlib import Path
-from typing import Literal
 from typing import List
 
 from fastapi import UploadFile
@@ -11,89 +10,90 @@ from app.repositories import AudioRepository
 from app.models import Audio
 from app.schemas.audio_schema import AudioCreate, AudioUpdate
 from app.exceptions import NotFoundException, AudioTypeNotSupportedException
-from app.facade import AudioInference
-from app.infrastructure import ModelDownloader
-
+from app.facade.audio_inference_facade import AudioInference, MODEL_CONFIG_MAP
 
 class AudioService:
+    __ALLOWED_CONTENT_TYPES = [
+        "audio/mpeg",
+        "audio/wav",
+        "audio/ogg",
+        "audio/flac",
+        "audio/aac",
+    ]
+
     def __init__(self) -> None:
         self.audio_repository = AudioRepository()
-        self.audio_facade = AudioInference()
-        self.model_downlaoder = ModelDownloader()
-        self.allowed_types = [
-            "audio/mpeg",
-            "audio/wav",
-            "audio/ogg",
-            "audio/flac",
-            "audio/aac",
-        ]
 
-    def find_by_id(self, id: UUID) -> Audio:
-        audio = self.audio_repository.find_by_id(id)
+    def get_by_id(self, id: UUID) -> Audio:
+        audio = self.audio_repository.get_by_id(id)
         if not audio:
             raise NotFoundException("Audio not found")
-
         return audio
 
-    def create(self, data: AudioCreate) -> Audio | None:
-        try:
-            self.audio_repository.create(data)
-        except Exception:
-            raise Exception("ERROR! Change message")
-
-    def upload(
-        self,
-        file: UploadFile,
-        user_id: UUID,
-        extraction_type: Literal["vocals", "instrumental", "4stems"]
-    ) -> List[Audio]:
-        if file.content_type not in self.allowed_types:
-            allowed_types_str = ", ".join(self.allowed_types)
+    def upload(self, file: UploadFile, user_id: UUID, pipeline: List[str]) -> List[Audio]:
+        if file.content_type not in self.__ALLOWED_CONTENT_TYPES:
+            allowed_types_str = ", ".join(self.__ALLOWED_CONTENT_TYPES)
             raise AudioTypeNotSupportedException(
                 f"Audio type not supported. Allowed types: {allowed_types_str}"
             )
 
-        audio_id = uuid4()
-        dir_path = Path(f"uploads/{user_id}/{audio_id}/")
-        dir_path.mkdir(parents=True, exist_ok=True)
-        file_path = dir_path / file.filename # pyright: ignore
+        for model_key in pipeline:
+            if model_key not in MODEL_CONFIG_MAP:
+                raise ValueError(f"Unsupported model in pipeline: {model_key}")
 
-        with file_path.open("wb") as buffer:
+        audio_id = uuid4()
+        base_dir = Path(f"uploads/{user_id}/{audio_id}/")
+        base_dir.mkdir(parents=True, exist_ok=True)
+        original_file_path = base_dir / file.filename
+
+        with original_file_path.open("wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
+        # Save original audio to DB
         audio = self.audio_repository.create(
             AudioCreate(
                 id=audio_id,
-                name=file.filename,  # pyright: ignore
-                data_path=str(file_path),
+                name=file.filename,
+                data_path=str(original_file_path),
                 user_id=user_id,
             )
         )
-
-        self.model_downlaoder.download_model(extraction_type,)
         created_audios = [audio]
 
-        output_files = self.audio_facade.vocal_inference(str(dir_path),extraction_type)
-        for output_file in output_files:
-            audio_file = AudioCreate(
-                id = uuid4(),
-                name=output_file["name"],
-                data_path=output_file["path"],
-                user_id=user_id,
-            )
-            created_audios.append(self.audio_repository.create(audio_file))
+        current_input_path = base_dir
+
+        for model_key in pipeline:
+            output_files = AudioInference.pipeline_inference(str(current_input_path), model_key)
+
+            created_this_step = []
+            for file_info in output_files:
+                audio_file = AudioCreate(
+                    id=uuid4(),
+                    name=file_info["name"],
+                    data_path=file_info["path"],
+                    user_id=user_id,
+                )
+                created_audio = self.audio_repository.create(audio_file)
+                created_this_step.append(created_audio)
+
+            created_audios.extend(created_this_step)
+
+            if output_files:
+                # Set input path for next model to the folder containing first output file
+                first_output_path = Path(output_files[0]["path"]).parent
+                current_input_path = first_output_path
+            else:
+                # No output, stop pipeline
+                break
 
         return created_audios
 
-
     def download(self, id: UUID, user_id: UUID):
-        audio = self.find_by_id(id)
-
+        audio = self.get_by_id(id)
         if not audio or audio.user_id != user_id:
             raise NotFoundException("Audio not found.")
 
         file_path = Path(audio.data_path)
-
         if not file_path.exists():
             raise NotFoundException("Audio file not found on disk")
 
@@ -104,15 +104,11 @@ class AudioService:
         )
 
     def update(self, data: AudioUpdate, id: UUID) -> Audio:
-        self.find_by_id(id)
-
-        audio_updated = self.audio_repository.update(data, audio)
-
+        self.get_by_id(id)
+        audio_updated = self.audio_repository.update(data, id)
         return audio_updated
 
     def delete(self, id: UUID) -> Audio:
-        self.find_by_id(id)
-
-        audio_deleted = self.audio_repository.delete(audio)
-
+        self.get_by_id(id)
+        audio_deleted = self.audio_repository.delete(id)
         return audio_deleted
